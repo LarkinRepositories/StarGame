9a9ecbf71dae7387e5c6e2daee7ff98730d96c4f
Dmitriy <larkingit@yandex.ru>
2019-10-29 15:28:05 +0700

    initial commit

---
+++ core/src/ru/larkinds/StarGame.java
@@ -0,0 +1,38 @@
+package ru.larkinds;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+public class StarGame extends ApplicationAdapter {
+	SpriteBatch batch;
+	Texture img;
+	TextureRegion region;
+
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		//img = new Texture("badlogic.jpg");
+		img = new Texture("Background/space_eye-wallpaper-2048x2048.jpg");
+		//region = new TextureRegion(img, 1000, 1000, 1024, 1024	);
+	}
+
+	@Override
+	public void render () {
+		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		batch.begin();
+		batch.draw(img, 0, 0, 2048, 2048);
+		//batch.draw(region, 50 , 50 , 1024, 1024);
+		batch.end();
+	}
+	
+	@Override
+	public void dispose () {
+		batch.dispose();
+		img.dispose();
+	}
+}
